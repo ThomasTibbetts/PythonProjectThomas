a380b949832e929700125364f3d15021979cd1d4
ThomasTibbetts <40066484+ThomasTibbetts@users.noreply.github.com>
2018-06-08 12:14:30 -0400

    Add files via upload

---
+++ negativeorpositive.py
@@ -0,0 +1,13 @@
+while True:
+    print("Input number.")
+    firstnumber = int(raw_input())
+    secondnumber = firstnumber
+    if secondnumber * -1 < firstnumber:
+        print("It is positive.")
+    else:
+        print("It is negative.")
+            
+    
+   
+        
+    
